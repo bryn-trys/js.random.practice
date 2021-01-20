e94ba27449b8aa1bf902f4bca461e926496b644a
Bryn <63167432+bryn-trys@users.noreply.github.com>
2021-01-20 12:12:03 -0800

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,10 @@
+@import url(https://fonts.googleapis.com/css?family=Raleway);
+.h1{
+font-family: raleway;  
+ font-size: 400%;
+ text-align: center;
+}
+
+body{
+background-image:url="https://s-media-cache-ak0.pinimg.com/736x/44/43/c5/4443c541cdc8f2cf8cd3b095a2065e76--wave-illustration-nautical-prints.jpg"
+}
